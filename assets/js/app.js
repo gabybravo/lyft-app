b70180b1a3d9227e36d403b4d259e43b67009848
Paula <paula.poncez@usach.cl>
2017-06-30 14:30:14 -0400

    generación de código aleatorio al hacer click en botón Next

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
-alert("holi");
+$(document).ready(function(){
+  $("#next-user").click(function(){
+    var text = "";
+    var possible = "0123456789";
+    var lab = "LAB-";
+    var res;
+    for (var i = 0; i < 3; i++){
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+      res = lab+text;
+    }
+    
+    alert(res);
+  });
+})
