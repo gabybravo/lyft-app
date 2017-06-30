ede81339e2b065b6908680dcc43ea9167d26dfa8
cstapiarubio <cstapiarubio@gmail.com>
2017-06-30 16:16:02 -0400

    estructura preliminar de la funcion localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
 alert("holi");
+
+$(document).ready(function(){      
+  
+  $(’#next-user’).click(function(){      
+   
+    /*Captura de código*/     
+
+var code=$(res).val();
+  /*Guardando los datos en el LocalStorage*/ 
+  localStorage.setItem(“Código”, code);   
+});   
+
+/*Funcion Cargar y Mostrar datos*/ 
+    
+  $(’#boton-cargar’).click(function(){ 
+
+    /*Obtener datos almacenados*/ 
+    var codigo = localStorage.getItem(“Código”); 
+
+  /*Mostrar datos almacenados*/    
+
+  $(codigo).html();   
+    });  
+}); 
