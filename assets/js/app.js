d1ac29e1da92e3b11d48f45cdd153eaf21b6332d
Paula Ponce <paula.poncez@usach.cl>
2017-07-02 16:32:49 -0400

    modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-  $("#next-user").click(function(){
-    var text = "";
-    var possible = "0123456789";
-    var lab = "LAB-";
-    var res;
-    for (var i = 0; i < 3; i++){
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-      res = lab+text;
-    }
-    
-    alert(res);
-  });
+	//Generación de código aleatorio
+	$("#next-user").click(function(){
+		var text = "";
+		var possible = "0123456789";
+		var lab = "LAB-";
+		var code;
+		for (var i = 0; i < 3; i++){
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+			code = lab + text;
+		}
+		alert("YOUR CODE IS: \n\n" + code);
+	});
+	
 })
