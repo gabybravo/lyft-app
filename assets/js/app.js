8104225902387ccbaea4fd3df3829d0ce0edf8ce
Paula Ponce <paula.poncez@usach.cl>
2017-07-02 16:48:50 -0400

    validación de código aleatorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,21 @@ $(document).ready(function(){
 		var text = "";
 		var possible = "0123456789";
 		var lab = "LAB-";
-		var code;
+		var code; //código
 		for (var i = 0; i < 3; i++){
 			text += possible.charAt(Math.floor(Math.random() * possible.length));
 			code = lab + text;
 		}
 		alert("YOUR CODE IS: \n\n" + code);
+
+		//Validación de código aleatorio
+		$("#confirm-btn").click(function(){
+			var inputVal = $("#confirm-code").val(); //datos del input en modal
+			if(inputVal != code){
+				alert("Invalid code.");
+			}else{
+				window.location.href="sign_up_user.html";
+			};
+		});
 	});
-	
-})
+});
