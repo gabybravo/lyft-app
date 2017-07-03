dde5dac1c82b604e223d05b27bd4d712065b3ed7
cstapiarubio <cstapiarubio@gmail.com>
2017-07-03 12:02:31 -0400

    funcion localstorage guarda el codigo aleatorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,21 @@
 
 $(document).ready(function(){      
   
-  //Generación de código aleatorio
-   var code; //código
-  $("#next-user").click(function(){
-    var text = "";
-    var possible = "0123456789";
-    var lab = "LAB-";
-   
-    for (var i = 0; i < 3; i++){
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-      code = lab + text;
-    }
-    alert("YOUR CODE IS: \n\n" + code);
+  //localStorage
+  $(window).unload(saveSettings);
+  loadSettings();
 
-    //Validación de código aleatorio
-    $("#confirm-btn").click(function(){
-      var inputVal = $("#confirm-code").val(); //datos del input en modal
-      if(inputVal != code){
-        alert("Invalid code.");
-      }else{
-        window.location.href="sign_up_user.html";
-      };
-
-    });
-    
   });
-
-   });
-
-$(document).ready(function(){  
-
-$("#next-user").click(function(){      
-   
-    /*Captura de código*/     
-
-/*var res=$(code).val();
- Guardando los datos en el LocalStorage 
-  localStorage.setItem(“Código”,res);*/
   
-  localStorage["Código"]=$(code).val();
+//Funciones localStorage
+function loadSettings() {
+  $('#code-country').val(localStorage.phone);
+  $("#confirmCode").val(localStorage.confirmCode);
+}
 
-   }); 
- }); 
-
-$(document).ready(function(){  
-  $("#boton-cargar").click(function(){ 
-
-    /*Obtener datos almacenados*/ 
-    var codigo = localStorage.getItem(“Código”); 
-
-alert(codigo);
-
-  /*Mostrar datos almacenados*/    
-
-  /*$(codigo).html(); */  
-    });    
-}); 
+function saveSettings() {
+  localStorage.phone = $('#code-country').val();
+  localStorage.confirmCode = $("#confirmCode").val();
+}
 
 
